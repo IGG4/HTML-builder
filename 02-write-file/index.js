9b8e837a7dd9b9a2ee43aeeb4ad8bf65c0e6e001
Irina Grigoryan <irina.g.grigoryan@gmail.com>
2025-01-19 22:47:54 +0400

    02-write-file

---
+++ 02-write-file/index.js
@@ -0,0 +1,31 @@
+const fs = require('fs');
+const path = require('path');
+const readline = require('readline');
+
+const filePath = path.join(__dirname, 'output.txt');
+const writeStream = fs.createWriteStream(filePath, { flags: 'a' });
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+console.log('Welcome! Please enter some text, and it will be written to the file.');
+console.log('Type "exit" to quit the program.');
+
+rl.on('line', (input) => {
+    if (input.toLowerCase() === 'exit') {
+        console.log('Goodbye!');
+        rl.close();
+        process.exit(0);
+    } else {
+        writeStream.write(input + '\n');
+        console.log('Text saved to file. Enter  text or type "exit" to quit.');
+    }
+});
+
+process.on('SIGINT', () => {
+    console.log('Goodbye!');
+    rl.close();
+    process.exit(0);
+});
